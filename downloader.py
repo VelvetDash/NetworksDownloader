dce5c35ab4062fa3a225c94d4b088428fe88bbde
Velvet <soon.wenbin@dhs.sg>
2018-11-02 10:39:33 +0800

    splitter function done

--- downloader.py
+++ downloader.py
@@ -1,8 +1,10 @@
 import urllib2,re
-##from queue import Queue
+import Queue
 ##from threading import Thread
+##from multiprocessing.dummy import Pool as ThreadPool
 
 numberOfThreads = 4
+
 
 class HeadRequest(urllib2.Request):
     def get_method(self):
@@ -24,17 +26,31 @@ def headQuery(url):
 
 #example: headQuery('https://i.redd.it/5wyehherasv11.jpg')
 
-def splitter(numBytes,threads):
-    arr=[]
+def splitter(numBytes,threads=numberOfThreads):
+    arr,arrOfTuples=[],[]
     n = int(numBytes/threads)
     for i in range(threads-1):
         arr.append(n)
     arr.append(numBytes-sum(arr))
+    for i in range(threads-1,-1,-1):
+        arr[i]+=sum(arr[:i])
+    start=0
+    for i in range(threads):
+        arrOfTuples.append((start,arr[i]))
+        start=arr[i]+1
+    return arrOfTuples
+
+#example: splitter(12345678,4)
 
 def thread(url,startByte,endByte):
     #format as dictionaries like {'Range':'bytes=0-99999'} and return tuple of dictionaries
     #max of 1mb per byteRange
     pass
+
+pool = ThreadPool(4) 
+results = pool.map(urllib2.urlopen, urls)
+pool.close() 
+pool.join()
 
 #???
 ##class Downloader(Thread):
