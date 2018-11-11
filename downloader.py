727a31dd2905c5c6d98be7830321cb23d88525ce
Velvet <soon.wenbin@dhs.sg>
2018-11-12 02:39:49 +0800

    added checker, fixer, test cases

--- downloader.py
+++ downloader.py
@@ -1,7 +1,6 @@
-import urllib2,re
+import urllib2,re,os
 import Queue
 from threading import Thread
-##from multiprocessing.dummy import Pool as ThreadPool
 
 numberOfThreads = 4
 url='https://i.redd.it/9orb8me3xpv11.jpg'         #image file
@@ -14,17 +13,19 @@ class HeadRequest(urllib2.Request):
         return "HEAD"
 
 def headQuery(url=url):
+    #gets number of bytes of the file at url
     req = HeadRequest(url)
     try:
         response = urllib2.urlopen(req)
         response.close()
     except Exception as e:
-        print e
+        print(e)
         return False
     try: return int(response.headers['Content-Length'])
     except: return False
 
 def splitter(numBytes,threads=numberOfThreads):
+    #splits bytes into 'threads' number of ranges
     arr,arrOfTuples=[],[]
     n = int(numBytes/threads)
     for i in range(threads-1):
@@ -37,8 +38,6 @@ def splitter(numBytes,threads=numberOfThreads):
         arrOfTuples.append((start,arr[i]))
         start=arr[i]+1
     return arrOfTuples
-
-#example: splitter(12345678,4)
 
 def thread(startByte,endByte):
     #format as dictionaries like {'Range':'bytes=0-999999'} and return tuple of dictionaries
@@ -53,24 +52,46 @@ def thread(startByte,endByte):
     dataDict[s]=data.read(endByte-s +1)
     data.close()
 
-byteSize = headQuery()
-if byteSize and byteSize>0:
-    args=splitter(byteSize)
-else:
-    raise Exception('cannot get number of bytes of the file in that URL')
+def checker():
+    #checks whether there are overlaps/ missing data/ excess data
+    keys=[]
+    length=0
+    for k,v in dataDict.iteritems():
+        keys.append(k)
+        length+=len(v)
+    for start in keys:
+        end=start+len(dataDict[start])-1
+        for i in keys:
+            #print('start:{} end:{} i:{}'.format(start,end,i)) #for debug
+            if i>start and i<=end:
+                print('start:{} end:{} i:{}'.format(start,end,i))
+                return False
+    if length!=byteSize:
+        print('length:{} byteSize:{}'.format(length,byteSize))
+        return False
+    return True
 
+def fixer():
+    #in-place fixes for problems detected by checker()
+    keys=sorted(dataDict.iterkeys(),reverse=True)
+    cutoff=byteSize
+    for k in keys:
+        #fix overlaps/ excess data
+        if k+len(dataDict[k])>cutoff:
+            truncBytes=dataDict[k][:cutoff-k-len(dataDict[k])]
+            dataDict[k]=truncBytes
+        #fix missing data
+        if k+len(dataDict[k])<cutoff:
+            req = urllib2.Request(url)
+            print('requesting bytes:{}-{}'.format(k+len(dataDict[k]), cutoff))
+            req.headers['Range'] = 'bytes=%s-%s' % (k+len(dataDict[k]), cutoff)
+            data = urllib2.urlopen(req)
+            dataDict[k+len(dataDict[k])]=data.read()
+        cutoff=k
+    
 
-threadPool=[]
-for i in range(numberOfThreads):
-    threadPool.append(Thread(name='thread'+str(i), target=thread, args=args[i]))
-for t in threadPool:
-    t.start()
-for t in threadPool:
-    t.join()
-
-
-import os
-def writer():
+def getFileName():
+    #returns a file name that is available
     fn=re.compile('([^/]+\.\w+$)')
     result=re.search(fn,url)
     if result:
@@ -80,10 +101,47 @@ def writer():
     while(os.path.exists(filename)):
         parts=filename.split('.')
         filename=parts[0]+'_1'+'.'+parts[1]
+    return filename
+
+def writer():
+    #writes data to file
+    filename=getFileName()
     sortedKeys=sorted(dataDict.keys())
     with open(filename,'wb') as f:
         for i in sortedKeys:
             f.write(dataDict[i])
-    print('{} saved at this script\'s folder'.format(filename))
+    sep = '\\' if os.name=='nt' else '/'
+    print('saved at {}{}{}'.format(os.getcwd(),sep,filename))
 
+byteSize = headQuery()
+if byteSize and byteSize>0:
+    args=splitter(byteSize)
+else:
+    raise Exception('cannot get number of bytes of the file in that URL')
+
+threadPool=[]
+for i in range(numberOfThreads):
+    threadPool.append(Thread(name='thread'+str(i), target=thread, args=args[i]))
+for t in threadPool:
+    t.start()
+for t in threadPool:
+    t.join()
+while not checker():
+    fixer()
 writer()
+
+
+###################################test cases
+##dataDict={0:'abcdefg',7:'abcdefg',14:'abcdefg',21:'abcdefg'}
+##byteSize=28
+##checker()
+##dataDict[7]='abc'
+##checker()
+##fixer()
+##dataDict[10]='defg'
+##checker()
+##
+##dataDict[10]='defghi'
+##checker()
+##fixer()
+##checker()
